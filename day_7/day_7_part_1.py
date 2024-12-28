1f98648595ff337fad8f795b5c9c5a6df5b9842a
William May <wjrm500@gmail.com>
2024-12-28 17:15:57 +0000

    Day 7 - Part 1 (very slow)

---
+++ day_7/day_7_part_1.py
@@ -0,0 +1,47 @@
+import copy
+import functools
+import itertools
+import os
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+with open(f"{script_dir}/day_7_input.txt") as f:
+    lines = f.readlines()
+
+@functools.cache
+def get_possible_operator_sets(num_operators: int) -> list[list[str]]:
+    possible_operator_sets = set()
+    for i in range(num_operators + 1):
+        s = ("+" * i) + ("*" * (num_operators - i))
+        for permutation in itertools.permutations(s, num_operators):
+            possible_operator_sets.add(permutation)
+    return [[y for y in x] for x in possible_operator_sets] # Listify
+
+def evaluate(operands: list[int], operator_set: list[str]) -> int:
+    operands = copy.copy(operands)
+    operator_set = copy.copy(operator_set)
+    cumulative_value = operands.pop(0)
+    while operands:
+        value = operands.pop(0)
+        operator = operator_set.pop(0)
+        cumulative_value = eval(f"{cumulative_value}{operator}{value}")
+    return cumulative_value
+
+def result_possible(expected_result: int, operands: list[int]) -> bool:
+    num_operators = len(operands) - 1
+    possible_operator_sets = get_possible_operator_sets(num_operators)
+    for operator_set in possible_operator_sets:
+        evaluated_result = evaluate(operands, operator_set)
+        if evaluated_result == expected_result:
+            return True
+    return False
+
+total = 0
+for line in lines:
+    expected_result, operands = line.split(": ")
+    expected_result = int(expected_result)
+    operands = list(map(int, operands.split()))
+    if result_possible(expected_result, operands):
+        total += expected_result
+print(total)
+# Answer: 975,671,981,569 - Correct
