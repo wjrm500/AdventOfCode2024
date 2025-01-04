7a4add4f986da050899d2bd0647b6bc04c628132
William May <wjrm500@gmail.com>
2025-01-04 12:32:08 +0000

    Day 12 - Part 2 (tidying up)

--- day_12/day_12_part_2.py
+++ day_12/day_12_part_2.py
@@ -13,7 +13,6 @@ class Point:
     x: int
     y: int
     region: "Region" | None
-    index: int
     boundary_edges: set[str]
 
     def __init__(self, plant_type: str, x: int, y: int) -> None:
@@ -24,40 +23,41 @@ class Point:
         self.boundary_edges = set()
 
 class Region:
-    points: list[Point]
+    matrix: list[list[Point]]
+    points_traversed: list[Point]
 
-    def __init__(self) -> None:
-        self.points = []
+    def __init__(self, matrix: list[list[Point]]) -> None:
+        self.matrix = matrix
+        self.points_traversed = []
 
     @property
     def area(self) -> int:
-        return len(self.points)
+        return len(self.points_traversed)
 
     @property
     def side_count(self) -> int:
-        return sum(len(point.boundary_edges) for point in self.points)
+        self.eliminate_collinear_boundary_edges()
+        return sum(len(point.boundary_edges) for point in self.points_traversed)
 
     @property
     def fence_price(self) -> int:
         return self.area * self.side_count
     
     def add_point(self, point: Point) -> None:
-        point.index = len(self.points)
         point.region = self
-        self.points.append(point)
+        self.points_traversed.append(point)
 
-    def get_neighbour_point_tuples(self, current_point: Point, matrix: list[list[Point]]) -> list[tuple[str, Point]]:
+    def get_neighbour_point_tuples(self, current_point: Point) -> list[tuple[str, Point]]:
         i, j = current_point.y, current_point.x
-        north_point = matrix[i - 1][j] if i > 0 else None
-        east_point = matrix[i][j + 1] if j < len(matrix[i]) - 1 else None
-        south_point = matrix[i + 1][j] if i < len(matrix) - 1 else None
-        west_point = matrix[i][j - 1] if j > 0 else None
+        north_point = self.matrix[i - 1][j] if i > 0 else None
+        east_point = self.matrix[i][j + 1] if j < len(self.matrix[i]) - 1 else None
+        south_point = self.matrix[i + 1][j] if i < len(self.matrix) - 1 else None
+        west_point = self.matrix[i][j - 1] if j > 0 else None
         return [("N", north_point), ("E", east_point), ("S", south_point), ("W", west_point)]
     
-    def recurse(self, current_point: Point, matrix: list[list[Point]]) -> None:
+    def recurse(self, current_point: Point) -> None:
         self.add_point(current_point)
-        neighbour_point_tuples = self.get_neighbour_point_tuples(current_point, matrix)
-
+        neighbour_point_tuples = self.get_neighbour_point_tuples(current_point)
         boundary_edges = set()
         for direction, neighbour_point in neighbour_point_tuples:
             if not neighbour_point or current_point.plant_type != neighbour_point.plant_type:
@@ -66,13 +66,13 @@ class Region:
                 continue
             if current_point.plant_type == neighbour_point.plant_type:
                 if neighbour_point.region is None:
-                    self.recurse(neighbour_point, matrix)
+                    self.recurse(neighbour_point)
         current_point.boundary_edges = boundary_edges
     
-    def eliminate_collinear_boundary_edges(self, matrix: list[list[Point]]) -> None:
-        self.points = sorted(self.points, key=lambda p: (p.y, p.x))
-        for point in self.points:
-            neighbour_point_tuples = self.get_neighbour_point_tuples(point, matrix)
+    def eliminate_collinear_boundary_edges(self) -> None:
+        self.points_traversed = sorted(self.points_traversed, key=lambda p: (p.y, p.x))
+        for point in self.points_traversed:
+            neighbour_point_tuples = self.get_neighbour_point_tuples(point)
             neighbour_points = [t[1] for t in neighbour_point_tuples]
             neighbour_boundary_edges = set()
             for neighbour_point in neighbour_points:
@@ -80,11 +80,9 @@ class Region:
                     neighbour_boundary_edges |= neighbour_point.boundary_edges
             collinear_boundary_edges = point.boundary_edges & neighbour_boundary_edges
             for collinear_boundary_edge in collinear_boundary_edges:
-                if point.plant_type == "J":
-                    print(f"Collinear boundary edge {collinear_boundary_edge} removed for point with plant_type {point.plant_type} at index {point.index}")
                 point.boundary_edges.remove(collinear_boundary_edge)
 
-
+# Find boundary edges
 matrix = [[Point(plant_type, x, y) for x, plant_type in enumerate(line)] for y, line in enumerate(lines)]
 regions: list[Region] = []
 for i in range(len(matrix)):
@@ -92,23 +90,9 @@ for i in range(len(matrix)):
     for j in range(len(line)):
         point = line[j]
         if not point.region:
-            region = Region()
-            region.recurse(point, matrix)
-            region.eliminate_collinear_boundary_edges(matrix)
+            region = Region(matrix)
+            region.recurse(point)
             regions.append(region)
 
-for line in matrix:
-    print(" ".join([(str(point.index).zfill(2) if point.plant_type == "J" else "..") for point in line]))
-    print()
-
-total_fence_price = 0
-for region in regions:
-    print("Region plant type:", region.points[0].plant_type)
-    print("Region area:", region.area)
-    print("Region side count:", region.side_count)
-    fence_price = region.fence_price
-    total_fence_price += fence_price
-    print("Region fence price:", region.fence_price)
-    print()
-print(total_fence_price)
+print(sum(region.fence_price for region in regions))
 # Answer: 908,042 - Correct
